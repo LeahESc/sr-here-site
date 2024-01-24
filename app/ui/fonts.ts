c888abeb49c2ee2b4879c3176ff900d42e9759eb
LeahESc <leahemily1@gmail.com>
2024-01-23 19:27:29 -0800

    Add fonts, restructure navbar

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Montserrat_Alternates, Agbalumo } from "next/font/google";
+
+export const monsterratAlternatives = Montserrat_Alternates({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const agbalumo = Agbalumo({
+  weight: ['400'],
+  subsets: ['latin'],
+});
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,42 +1,40 @@
 import Link from 'next/link';
 // import { PowerIcon } from '@heroicons/react/24/outline';
-
+import { agbalumo } from './fonts';
 export default function NavBar() {
   const links = [
-    { name: 'Home',
+    { name: 'HOME',
       href: '/', 
       // icon: HomeIcon 
     },
     {
-      name: 'About',
+      name: 'ABOUT',
       href: '/about',
       // icon: DocumentDuplicateIcon,
     },
     { 
-      name: 'Work',
+      name: 'WORK',
       href: '/work',
       // icon: UserGroupIcon 
     },
     { 
-      name: 'Pickle',
+      name: 'PICKLE',
       href: '/pickle_ball',
       // icon: UserGroupIcon 
     },
   ];
   return (
-    <nav className="bg-violet-100 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-right justify-between mx-auto p-4">
-        <div className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          {links.map(link => (
+    <nav className="bg-violet-100 mx-auto p-3 justify-content-end fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+      <div className="flex place-content-end space-x-8 p-4 md:p-0 font-medium sm:space-x-2 md:space-x-8">
+        {links.map(link => (
             <Link 
               key={link.name}
               href={link.href}
-              className="flex text-blue-600"
+              className="flex inline-block text-violet-400 place-self-end"
             >
-              <p className="">{link.name}</p>
+              <p className={`${agbalumo.className} text-3xl hover:text-violet-600`}>{link.name}</p>
             </Link>
-          ))}
-        </div>
+        ))}
       </div>
   </nav>
   );
